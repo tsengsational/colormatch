68f9e177b0e8f438628756fccc8cf6f551ee9826
github username <github email address>
2017-10-04 17:48:24 -0400

    clean up

--- app.js
+++ app.js
@@ -18,38 +18,17 @@ let icon = document.querySelector('.fa')
 icon.addEventListener('click', setRandomColor)
 
 document.onmousemove = function(e) {
-      var mouseCoords = getMousePos(e);
-      // console.log(mouseCoords)
-      let h = parseInt((mouseCoords["x"] / width) * 360)
-      let l = parseInt((mouseCoords["y"] / height) * (50 - 20) + 20)
-      console.log(l)
+  var mouseCoords = getMousePos(e);
+  // console.log(mouseCoords)
+  let h = parseInt((mouseCoords["x"] / width) * 360)
+  let l = parseInt((mouseCoords["y"] / height) * (50 - 20) + 20)
 
-      document.documentElement.style.setProperty('--base', `hsl(${h}, 50%, ${l}%)`)
+  document.documentElement.style.setProperty('--base', `hsl(${h}, 50%, ${l}%)`)
 
-      let base = document.documentElement.style.getPropertyValue('--base')
-      let target = document.documentElement.style.getPropertyValue('--target')
-      console.log(target, base)
-      if ( base === target) {
-        alert("You matched!")
-        setRandomColor()
-      }
+  let base = document.documentElement.style.getPropertyValue('--base')
+  let target = document.documentElement.style.getPropertyValue('--target')
+  if ( base === target) {
+    alert("You matched!")
+    setRandomColor()
+  }
  };
-
- document.addEventListener('touchmove', handleMove)
-
-function handleMove(e) {
-       var touchCoords = getMousePos(e);
-       console.log(touchCoords)
-       let h = parseInt((touchCoords["x"] / width) * 360)
-       let l = parseInt((touchCoords["y"] / height) * (50 - 20) + 20)
-
-       document.documentElement.style.setProperty('--base', `hsl(${h}, 50%, ${l}%)`)
-
-       let base = document.documentElement.style.getPropertyValue('--base')
-       let target = document.documentElement.style.getPropertyValue('--target')
-       console.log(target, base)
-       if ( base === target) {
-         alert("You matched!")
-         setRandomColor()
-       }
-  };
